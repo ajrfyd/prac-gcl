0fcd35b0cab58d606295de1b727b668361f1f846
ajrfyd <ajrfyd@naver.com>
2022-05-20 17:44:57 +0900

    Complete: complete of practice

--- README.md
+++ README.md
@@ -10,3 +10,27 @@ Better than REST API
 2. Underfetching 
 - 필요한 정보가 여러개 있으며, 다른 정보는 다른 엔드포인트에 있을때, 요청을 두번 해야 한다. 
 - 하지만 graph-ql은 한번의 요청으로 처리가 가능하다. 
+
+---
+
+### 기본 Setting 방법
+
+```js
+  import { ApolloServer, gql } from 'apollo-server';
+  import fetch from 'node-fetch';
+  const server = new ApolloServer({ typeDefs, resolvers });
+
+  server.listen().then(({adress, family, url, port, server}) => {
+    console.log(adress, family, url, port)
+  })
+```
+
+### typeDefs
+- 기본적으로 type Query { } 는 존재해야함. 
+- Query 않의 key 값이 요청 키가 된다. 
+- resolver 해결사? 요청을 처리하는 해결사 그런 느낌인듯 
+- resolver 안의 Query는 get 요청 처리에 대한 함수들
+- mutation은 post, put, delete와 같은 처리 함수들을 정의 
+- resolve 안의 User와 Tweet:
+  - type User: user 정보 요청 중 info라는 정의 되지 않은 것을 요청하면 reoslver에서 찾는다. resolver > User > info함수에 정의된 함수에는 첫번째 Argument로 user에서 리턴된 obj의 정보들이 들어온다. 따라서 user 에 없는 info를  요청하더라도 정의된 info함수에서 리턴하는 값을 얻을 수 있다. 
+  - type Tweet: 마찬가지 이다. tweet에 author가 없다는 것을 알고 graph-ql은 resolver 어딘가에 resolver가 있어야 한다는 것을 앎. argument로 tweets의 데이터가 들어오고 userId로 users에 있는 id와 일치하는 데이터 찾아 반환함.
--- server.js
+++ server.js
@@ -1,31 +1,145 @@
 import { ApolloServer, gql } from 'apollo-server';
-
+import fetch from 'node-fetch';
 
 // GET /text
 // GET /hello
 // GET /allFilms
 
+let tweets = [
+  {
+    id: '1',
+    text: 'first data',
+    userId: '2'
+  },
+  {
+    id: '2',
+    text: 'second Data', 
+    userId: '1'
+  }
+]
+
+let users = [
+  {
+    id: '1',
+    username: 'ajryd',
+    age: 32,
+    height: 182
+  },
+  {
+    id: '2',
+    username: 'Elon musk',
+    age: 51,
+    height: 190
+  }
+]
+
 const typeDefs = gql`
   type User {
-    id: ID!,
-    username: String!,
-    age: Number!
+    id: ID!
+    username: String!
+    age: Int!
+    height: Int!
+    """
+    Info is username & age & height combination
+    """
+    info: String!
   }
+  """
+  Tweet object represents a resource for a Tweet!
+  """
   type Tweet {
-    id: ID!,
-    text: String!,
-    author: User!
+    id: ID!
+    text: String!
+    author: User
   }
   type Query {
     allTweets: [Tweet!]!
     tweet(id: ID!): Tweet
+    ping: String!
+    allUsers: [User!]!
+    allMovies: [Movie!]!
+    movie(id: String!): Movie
   }
   type Mutation {
     postTweet(text: String!, userId: ID!): Tweet!
     deleteTweet(id: ID!): Boolean!
   }
+  type Movie {
+    id: Int!
+    url: String!
+    imdb_code: String!
+    title: String!
+    title_english: String!
+    title_long: String!
+    slug: String!
+    yearrating: Float!
+    runtime: Float!
+    genres: [String]!
+    summary: String!
+    description_full: String!
+    synopsis: String!
+    yt_trailer_code: String!
+    language: String!
+    background_image: String!
+    background_image_original: String!
+    small_cover_image: String!
+    medium_cover_imagelarge_cover_image: String!
+  }
 `
-const server = new ApolloServer({ typeDefs });
+
+const resolvers = {
+  Query: {
+    tweet(root, args) {
+      console.log('%cCalled!', 'color: red')
+      console.log(root, args)
+      return tweets.filter(tweet => tweet.id === args.id)[0];
+    },
+    ping: () => {
+      return 32 
+    },
+    allTweets: () => {
+      return tweets;
+    },
+    allUsers: () => {
+      return users;
+    },
+    allMovies: () => {
+      return fetch("https://yts.mx/api/v2/list_movies.json").then(r => r.json()).then(json => json.data.movies)
+    },
+    movie: (_, { id }) => {
+      return fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`).then(r => r.json()).then(json => json.data.movie)
+    }
+  },
+  Mutation: {
+    postTweet: (_, { text, userId }) => {
+      const newTweet = {
+        id: tweets.length + 1,
+        text: text,
+        userId,
+      }
+      tweets.push(newTweet);
+      return newTweet;
+    },
+    deleteTweet: (_, { id }) => {
+      const tweet = tweets.find(tweet => tweet.id === id);
+      if(!tweet) return false;
+      tweets = tweets.filter(tweet => tweet.id !== id);
+      return true;
+    },
+  },
+  User: {
+    info: ({ username, age, height }) => {
+      return `${username} is ${age} Year Old & ${height} cm tall`
+    } 
+  },
+  Tweet: {
+    author: ({ userId }) => {
+      return users.find(user => user.id === userId);
+    }
+  }
+}
+
+const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({adress, family, url, port, server}) => {
   console.log(adress, family, url, port)
